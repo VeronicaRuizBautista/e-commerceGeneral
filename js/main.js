cabe2139ad9889985de8efebef95a0aa5321a23c
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2024-03-21 17:48:31 -0500

    feat: :sparkles: Se agrego la funcion para restar la cantidad de productos

--- js/main.js
+++ js/main.js
@@ -7,3 +7,9 @@ function botonDetalles() {
     var btn = document.getElementById("aviso");
     btn.classList.toggle("verdetalles");
     }
+
+function restar(id) {
+    let contador = parseInt(document.getElementById(id).innerText);
+    contador--;
+    document.getElementById(id).innerText = contador;
+}
