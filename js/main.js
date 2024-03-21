8f6e3eefb616596f343720e841cbed3e1e182f34
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2024-03-21 17:32:15 -0500

    feat: :sparkles: Se agrego el aviso
    Se agrego el aviso de ver detalles

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ function botonMenu() {
     var btn = document.getElementById("menu");
     btn.classList.toggle("visible");
     }
+
+function botonDetalles() {
+    var btn = document.getElementById("aviso");
+    btn.classList.toggle("verdetalles");
+    }
